873d95acfee282bf5676babe975f7492e980abc1
noob-br <marciors05@hotmail.com>
2026-09-02 17:17:19 -0300

    chore: remove loose TesteQuestao1 and keep only questao1 package

--- 02.09/TesteQuestao1.java
+++
@@ -1,8 +0,0 @@
-public class TesteQuestao1 {
-    public static void main(String[] args) {
-
-        int teste = AvaliacaoDS1p1.quantidadeTriangulosPitagoricos(50, 1000);
-        System.out.println(teste);
-
-    }
-}
